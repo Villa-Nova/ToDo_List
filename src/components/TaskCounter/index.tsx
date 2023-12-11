bfe083d87481c1c52d824035fbd8e57947ed1b95
Mateus Villa Nova <villa1999nova@gmail.com>
2023-12-11 13:31:51 -0300

    feat: Addition counter created

--- src/components/TaskCounter/index.tsx
+++ src/components/TaskCounter/index.tsx
@@ -1,11 +1,18 @@
 import styled from "./TaskCounter.module.css";
 
-export function TaskCounter() {
+interface TaskCounterProps {
+  searchValueArray: string[]
+}
+
+export function TaskCounter({ searchValueArray }: TaskCounterProps) {
+
   return (
     <div className={styled.flex}>
       <p className={styled.font_blue}>
         Tasks created
-        <span className={styled.circle}>0</span>
+        <span className={styled.circle}>
+          {searchValueArray.length}
+        </span>
       </p>
 
       <p className={styled.font_purple}>
