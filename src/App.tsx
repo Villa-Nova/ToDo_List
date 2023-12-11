ea5018da5f14a665492692c8bca3ba8f14a5cb2d
Mateus Villa Nova <villa1999nova@gmail.com>
2023-12-11 13:28:32 -0300

    refactor: functionsand feat: created new features

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,57 @@ import { useState } from "react"
 import styles from "./App.module.css"
 
 import { Header } from "./components/Header"
-/* import { NoTask } from "./components/NoTask" */
 import { Search } from "./components/Search"
 import { TaskList } from "./components/TaskList"
 import { TaskCounter } from "./components/TaskCounter"
+import { NoTask } from "./components/NoTask"
 
 export function App() {
-  const [searchValue, setSearchValue] = useState<string | undefined>("");
+  const [, setSearchValue] = useState<string | undefined>("");
+  const [searchValueArray, setSearchValueArray] = useState<string[]>(() => {
+    const storedTasks = localStorage.getItem('tasks');
+    return storedTasks ? JSON.parse(storedTasks) : [];
+  });
 
-  function handleSearchSubmit(value: string) {
+  const updateLocalStorage = (tasks: string[]) => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  };
+
+  const handleSearchSubmit = (value: string) => {
     setSearchValue(value);
-    console.log(searchValue);
-  }
+    const newTasks = [...searchValueArray, value];
+    setSearchValueArray(newTasks);
+    updateLocalStorage(newTasks);
+  };
+
+  const handleDelete = (taskToDelete: string) => {
+    const newTasks = searchValueArray.filter(taskArray => taskArray !== taskToDelete);
+    setSearchValueArray(newTasks);
+    updateLocalStorage(newTasks);
+  };
 
   return (
     <>
       <Header />
 
       <main className={styles.center}>
-        <Search handleSubmit={handleSearchSubmit} />
-        <TaskCounter />
-        <TaskList />
+        <Search 
+          handleSubmit={handleSearchSubmit} 
+        />
+        <TaskCounter 
+          searchValueArray={searchValueArray} 
+        />
+        
+        {
+          searchValueArray.length === 0 ? (
+            <NoTask /> 
+          ) : (
+            <TaskList 
+              searchValueArray={searchValueArray}
+              onDelete={handleDelete}
+            />
+          )
+        }
       </main>
     </>
   )
